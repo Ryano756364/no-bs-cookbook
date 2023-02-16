751dfa1d665188f7ff0f74d5c232456a083b44ed
rodell <ryanodell2668@gmail.com>
2023-02-16 08:01:56 -0500

    Error page added

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,39 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
 function ErrorPage() {
+
+  //page 100 and btn in global css setup to avoid redundancy in css styling
   return (
-    <div>ErrorPage</div>
+    <Wrapper className='page-100'>
+      <section>
+        <h1>
+          404
+        </h1>
+        <h3>Sorry, page doesn't exist.</h3>
+        <Link to="/" className='btn'>
+          Head back home
+        </Link>
+      </section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  h1 {
+    font-size: 10rem;
+  }
+
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`
 
 export default ErrorPage
