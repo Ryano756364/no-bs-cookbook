3b4b8d38fc9f046962ff3f87587c0d73b82d5154
rodell <ryanodell2668@gmail.com>
2023-02-18 15:54:59 -0500

    Favorite columnns added

--- src/components/FavoriteColumns.js
+++ src/components/FavoriteColumns.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import styled from 'styled-components';
 
-function FavoriteColumns() {
+const FavoriteColumns = () => {
   return (
-    <div>FavoriteColumns</div>
+    <Wrapper>
+      <div className='content'>
+        <h5>Pretty Photo</h5>
+        <h5>Recipe Name</h5>
+        <h5>Servings</h5>
+        <h5>Happy People</h5>
+        <span></span> {/* delete button */}
+      </div>
+      <hr />
+    </Wrapper>
   )
 }
 
-export default FavoriteColumns
+const Wrapper = styled.div`
+  display: none;
+
+  @media (min-width: 776px) {
+    display: block;
+    .content {
+      display: grid;
+      grid-template-columns: 316px 1fr 1fr 1fr auto;
+      justify-items: center;
+      column-gap: 1rem;
+      h5 {
+        color: var(--clr-grey-5);
+        font-weight: 400;
+      }
+    }
+
+    span {
+      width: 2rem;
+      height: 2rem;
+    }
+
+    hr {
+      margin-top: 1rem;
+      margin-bottom: 3rem;
+    }
+  }
+`
+
+export default FavoriteColumns;
